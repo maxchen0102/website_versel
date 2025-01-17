46218ad0058a949380700a3b0009822a134ab21a
Chris.Chen <Chris.Chen@splashtop.com>
2025-01-17 19:57:35 +0800

    PA test v3

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,15 @@ export default defineConfig({
   base: '/',
   plugins: [
       vue(),
-      VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '您的應用名稱',
+        short_name: '短名稱',
+        theme_color: '#ffffff',
+        display: 'standalone', // 這會隱藏瀏覽器的網址列
+        start_url: '/'
+      }
+    })
   ],
 })
