b7c981b8a1a1257f7354022c196ac82cbc557887
Chris.Chen <Chris.Chen@splashtop.com>
2025-01-17 19:34:07 +0800

    PWA test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      VitePWA({ registerType: 'autoUpdate' })
+  ],
 })
